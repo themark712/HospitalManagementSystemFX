2542ef381627f8bb3c19afb7a3d717a0d4894153
cni-mark <cni-mark@ProDesk600-Mark>
2025-10-13 11:25:06 -0500

    reboot

--- src/hospitalmanagementsystemfx/FXMLDocumentController.java
+++ src/hospitalmanagementsystemfx/FXMLDocumentController.java
@@ -7,28 +7,91 @@ package hospitalmanagementsystemfx;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author cni-mark
  */
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
+  // name all components on admin page
+  @FXML
+  private Button buttonLogin;
+
+  @FXML
+  private Button buttonRegister;
+
+  @FXML
+  private CheckBox checkLoginShowPassword;
+
+  @FXML
+  private CheckBox checkRegisterShowPassword;
+
+  @FXML
+  private AnchorPane formLogin;
+
+  @FXML
+  private AnchorPane formMain;
+
+  @FXML
+  private AnchorPane formRegister;
+
+  @FXML
+  private Label label;
+
+  @FXML
+  private Label label1;
+
+  @FXML
+  private Label labelLogin;
+
+  @FXML
+  private Label labelRegister;
+
+  @FXML
+  private Hyperlink linkLoginHere;
+
+  @FXML
+  private Hyperlink linkRegisterHere;
+
+  @FXML
+  private PasswordField textLoginPassword;
+
+  @FXML
+  private TextField textLoginShowPassword;
+
+  @FXML
+  private TextField textLoginUsername;
+
+  @FXML
+  private TextField textRegisterEmail;
+
+  @FXML
+  private PasswordField textRegisterPasswod;
+
+  @FXML
+  private TextField textRegisterShowPassword;
+
+  @FXML
+  private TextField textRegisterUsername;
+
+  public void switchForm(ActionEvent event) {
+    if (event.getSource() == linkRegisterHere) {
+      formLogin.setVisible(false);
+      formRegister.setVisible(true);
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    else if (event.getSource() == linkLoginHere) {
+      formRegister.setVisible(false);
+      formLogin.setVisible(true);
+    }
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }
+
 }
--- src/hospitalmanagementsystemfx/HospitalManagementSystemFX.java
+++ src/hospitalmanagementsystemfx/HospitalManagementSystemFX.java
@@ -8,8 +8,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 /**
  *
  * @author cni-mark
@@ -21,6 +21,8 @@ public class HospitalManagementSystemFX extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        Image icon = new Image("hospital.png");
+        stage.getIcons().add(icon);
         
         stage.setScene(scene);
         stage.show();
